0aa2827914f2116e2726b67786bdff8bc6cbc4bd
Mary Lang <mcl86@cornell.edu>
2020-02-20 11:26:35 -0700

    Testing for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./src/index.js', './src/stylesheets/styles.scss'],
+  entry: ['index.js', './src/stylesheets/styles.scss'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'docs')
